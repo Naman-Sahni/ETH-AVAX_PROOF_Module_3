9e55d3b6625a713362899ed3c2a746e87b2f8ad0
Naman-Sahni <146383931+Naman-Sahni@users.noreply.github.com>
2024-07-01 00:24:48 +0530

    Update ETH+AVAX_PROOF_Module3_Assessment.sol

--- ETH+AVAX_PROOF_Module3_Assessment.sol
+++ ETH+AVAX_PROOF_Module3_Assessment.sol
@@ -12,7 +12,6 @@ contract MyToken {
     mapping(address => mapping(address => uint256)) public allowances;
 
     event Transfer(address indexed from, address indexed to, uint256 value);
-    event Approval(address indexed owner, address indexed spender, uint256 value);
     event Mint(address indexed to, uint256 value);
     event Burn(address indexed from, uint256 value);
 
@@ -40,14 +39,7 @@ contract MyToken {
         emit Transfer(msg.sender, _to, _value);
     }
 
-    function approve(address _spender, uint256 _value) external {
-        require(_spender != address(0), "Invalid spender address");
-
-        allowances[msg.sender][_spender] = _value;
-
-        emit Approval(msg.sender, _spender, _value);
-    }
-
+  
     function transfer_from(address _from, address _to, uint256 _value) external {
         require(_from != address(0), "Invalid sender address");
         require(_to != address(0), "Invalid recipient address");
@@ -81,22 +73,6 @@ contract MyToken {
         emit Burn(msg.sender, _value);
     }
 
-    function increase_allowance(address _spender, uint256 _added_value) external {
-        require(_spender != address(0), "Invalid spender address");
-
-        allowances[msg.sender][_spender] += _added_value;
-
-        emit Approval(msg.sender, _spender, allowances[msg.sender][_spender]);
-    }
-
-    function decrease_allowance(address _spender, uint256 _subtracted_value) external {
-        require(_spender != address(0), "Invalid spender address");
-        uint256 current_allowance = allowances[msg.sender][_spender];
-        require(current_allowance >= _subtracted_value, "Decreased allowance below zero");
-
-        allowances[msg.sender][_spender] = current_allowance - _subtracted_value;
-
-        emit Approval(msg.sender, _spender, allowances[msg.sender][_spender]);
-    }
+  
 
 }
